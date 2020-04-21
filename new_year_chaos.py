7bc75130b1cd5004510111ddce652382fee6685d
tbremm <timothy.bremm@gmail.com>
2020-04-20 22:26:02 -0700

    stub for NYC, simplified rotate_left

---
+++ new_year_chaos.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=arrays&isFullScreen=true&h_r=next-challenge&h_v=zen
+
+
+def minimumBribes(q):
+    # Start from beginning of the array, using the final state as starting point
+    num_bribes = 0
+
+
+    return num_bribes
+
+
+start = [2, 1, 5, 3, 4]
+print(minimumBribes(start))
--- rotate_arr_left.py
+++ rotate_arr_left.py
@@ -1,12 +1,13 @@
 # Rotate an array of length arr_length to the left "rotations" number of times
+# https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=arrays&isFullScreen=true
 
 
 def rot_left(a, d):
     length = len(a)
-    return a[d % length:length] + (a[:d % len(a)])
+    return a[d % length:] + (a[:d % length])
 
 
-rotations = 5
+rotations = 10
 arr_length = 8
 arr = [i for i in range(arr_length)]
 for r in range(rotations):
