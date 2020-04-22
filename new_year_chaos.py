731113582fb099a249b53b6680cd61e53754adf1
tbremm <timothy.bremm@gmail.com>
2020-04-21 19:22:30 -0700

    nyc completed

--- new_year_chaos.py
+++ new_year_chaos.py
@@ -2,12 +2,27 @@
 
 
 def minimumBribes(q):
-    # Start from beginning of the array, using the final state as starting point
     num_bribes = 0
+    for i, p in enumerate(q):
+        if p - 1 - i > 2:
+            print("Too chaotic")
+            return
+        for j in range(max(p - 2, 0), i):
+            if q[j] > p - 1:
+                num_bribes += 1
+    print(num_bribes)
+    return
 
 
-    return num_bribes
+good0 = [2, 1, 5, 3, 4]  # 3
+too_chaotic0 = [2, 5, 1, 3, 4]
+minimumBribes(good0)
+minimumBribes(too_chaotic0)
 
+good1 = [1, 2, 5, 3, 7, 8, 6, 4]  # 7
+too_chaotic1 = [5, 1, 2, 3, 7, 8, 6, 4]
+minimumBribes(good1)
+minimumBribes(too_chaotic1)
 
-start = [2, 1, 5, 3, 4]
-print(minimumBribes(start))
+good2 = [1, 2, 5, 3, 4, 7, 8, 6]  # 4
+minimumBribes(good2)
