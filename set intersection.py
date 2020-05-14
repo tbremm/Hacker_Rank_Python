f7e1b336bd658ae12c7bac7e4dfac25d0a3bbd6a
tbremm <timothy.bremm@gmail.com>
2020-05-13 21:59:21 -0700

    finished set intersection

---
+++ set intersection.py
@@ -0,0 +1,31 @@
+# Task
+# The students of District College have subscriptions to English and French newspapers. Some students have subscribed
+# only to English, some have subscribed only to French, and some have subscribed to both newspapers.
+#
+# You are given two sets of student roll numbers. One set has subscribed to the English newspaper, one set has
+# subscribed to the French newspaper. Your task is to find the total number of students who have subscribed to both
+# newspapers.
+#
+# Input Format
+# The first line contains n, the number of students who have subscribed to the English newspaper.
+# The second line contains n space separated roll numbers of those students.
+# The third line contains b, the number of students who have subscribed to the French newspaper.
+# The fourth line contains b space separated roll numbers of those students.
+#
+# Constraints
+# 0 < total students < 1000
+#
+# Output Format
+# Output the total number of students who have subscriptions to both English and French newspapers.
+
+# n = int(input())
+# english = set(map(int, input().split()))
+# b = int(input())
+# french = set(map(int, input().split()))
+
+n = 9
+english = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+b = 9
+french = {10, 1, 2, 3, 11, 21, 55, 6, 8}
+
+print(len(english.intersection(french)))
